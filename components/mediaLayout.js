e12fed8b5c3214001d4b4991c9fdc77fb73c1038
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-05-25 16:57:43 +0200

    updates

--- components/mediaLayout.js
+++ components/mediaLayout.js
@@ -14,7 +14,7 @@ const MediaLayout = ({ images, youtubeLinks, videos, mixcloudLinks }) => {
       <div className={`row ${Media.images_row}`}>
         <div className="col-xl-12 col-md-8 col-sm-12 col-12 ">
           <img
-            src="https://res.cloudinary.com/hr3oe8cre/image/upload/v1621933067/dots2_6199d03242.gif"
+            src="https://res.cloudinary.com/hr3oe8cre/image/upload/v1621954575/initial_grip_dots_1dedbb02a9.gif"
             className={`${
               images.length > 0 ? Media.dots_style_for_images : Media.dots_style
             }`}
