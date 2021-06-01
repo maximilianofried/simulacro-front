27fca811992ee878f635be95ec3d6f743472f675
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-06-02 01:15:51 +0200

    updates

--- components/terminal.js
+++ components/terminal.js
@@ -139,26 +139,19 @@ const Terminal = ({ content, contentList, type, id, tags }) => {
         </>
       ) : (
         <>
-          <p className={`${ContentLayout.simulacro_description}`}>
-            Founded in 2019 by{' '}
-            <Link href="/collaborators/ignacio-bruno">
-              <a className={ContentLayout.color_black}>Ignacio Bruno</a>
-            </Link>
-            ,{' '}
-            <Link href="/collaborators/javier-rojas">
-              <a>Javier Rojas</a>
-            </Link>{' '}
-            and{' '}
-            <Link href="/collaborators/maximiliano-fried">
-              <a>Maximiliano Fried</a>
-            </Link>
+          <textarea
+            rows="8"
+            cols="70"
+            className={`${ContentLayout.simulacro_description}`}
+          >
+            Founded in 2019 by Ignacio Bruno, Javier Rojas and Maximiliano Fried
             , Simulacro is a transdisciplinary platform based between Berlin
             (DE) and Córdoba (AR). Working closely with artists and musicians we
             create multisensorial experiences, websites, and experimental
             interfaces. With a background in architecture, design, engineering,
             software and art our practice links analogue and digital space,
             aural and visual languages.
-          </p>
+          </textarea>
           <p>
             We use software technologies like: Javascript - Python - C++ - Max
             MSP + Ambisonics - Jitter - Touch designer - Resolume Arena -
